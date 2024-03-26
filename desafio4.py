664385dd2b703fc2013864e107ac492a987e0854
Revecksouls <161537596+Revecksouls@users.noreply.github.com>
2024-03-26 14:07:13 -0300

    Add files via upload

---
+++ desafio4.py
@@ -0,0 +1,8 @@
+print("Nota final")
+
+n1 = float(input("primeira prova:"))
+n2 = float(input("segunda prova:"))
+media = (n1 + n2) / 2
+print("Sua nota final foi:",media)
+
+
---
+++ outros_operadores.py
@@ -0,0 +1,8 @@
+num1 = int(input("digite um  valor:"))
+num2 = int(input("digite um  valor:"))
+
+modulo = num1 % num2
+print(modulo)
+
+nome = "pablo"
+print(type(nome))
