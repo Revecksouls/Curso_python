a1874455caa55bf7d8821241ec97a115127a2542
Revecksouls <pablogabrielribeiroferreira@gmail.com>
2024-03-27 16:37:51 -0300

    atualização1.0

--- Strings_e_desafios/desafio2.py
+++ Strings_e_desafios/desafio2.py
@@ -1,7 +1,12 @@
-cidade1 = "Espirito Santo"
-cidade2 = "Rio de Janeiro"
-cidade3 = "Santa Catarina"
+#cidade1 = "Espirito Santo"
+#cidade2 = "Rio de Janeiro"
+#cidade3 = "Santa Catarina"
 
-print("Santo" in cidade1)
-print("Santo" in cidade2)
-print("Santo" in cidade3)
+#print("Santo" in cidade1)
+#print("Santo" in cidade2)
+#print("Santo" in cidade3)
+
+cidade = input("digite a cidade:")
+primeira = cidade.split()
+print( "santo" in primeira[0])
+ 
--- Strings_e_desafios/desafio5.py
+++ Strings_e_desafios/desafio5.py
@@ -3,3 +3,4 @@ nome_compl = nome.split()
 print(nome_compl[0])
 ultimo_nome = nome.split()
 print(ultimo_nome[-1])
+
