963896e3091614faa1ddd72526b94c06362f6b8a
Pablo Gabriel <pablogabrielribeiroferreira@gmail.com>
2024-04-10 16:56:33 -0300

    Atualização 1.8

--- while/desafio4.py
+++ while/desafio4.py
@@ -9,20 +9,43 @@ ganhando_par = 0
 # escolha do usuario
 while True:
     escolha_Usuario = str(input("escolha I/P: ")).upper()
-    escolha_numero_par = int(input("Digite um numero: "))
-    par = escolha_numero_par % 2
-    if par == 0:
-        print(escolha_numero_par)
+    escolha_numero_usuario = int(input("Digite um numero: "))
+    
+       # print(escolha_numero_usuario)
     
     #maquina caminho impar 
     lista = randint(1,10)
     maquina = lista 
-    impar = lista % 3
     print(lista)
     
     # ganhando da maquina
-    ganhando_par = escolha_numero_par + lista 
+    soma = escolha_numero_usuario + lista 
+    resto = soma % 2
     
-    if ganhando_par % 2:
-        par = 0
-        print(ganhando_par, "resultado par" )
+    if escolha_Usuario == "P" and resto == 0 : 
+
+        print(soma, "resultado par, voce ganhou" )
+    else:
+        print(soma,"resultado impar , maquina ganhou")
+        break
+    
+    if escolha_Usuario == "I" and resto == 1 : 
+        print(soma, "resultado impar, voce ganhou" )
+    else:
+        print(soma,"resultado par , maquina ganhou")
+        
+        break
+    
+    
+    
+    # if  escolha_Usuario == "I":
+    #     escolha_numero_usuario = ganhando_impar % 2
+    #     ganhando_impar = escolha_numero_usuario + lista
+        
+    #     print(escolha_numero_usuario)
+    #     ganhando_impar % 2 
+    #     impar = 1
+    #     print(ganhando_impar, "voce ganhou")
+    # else:
+    #     print("resultado par , maquina ganhou")
+    #     break
